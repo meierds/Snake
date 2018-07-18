0acf4ce488b19d76ecba5d9243046b6512ab0943
David Meier <davidscottmeier@yahoo.com>
2018-07-18 07:29:12 -0400

    Added files.

---
+++ Food animation.js
@@ -0,0 +1,35 @@
+const foodImg = new Image();
+//foodImg.src = ;
+
+
+function sprite (srcImage) {
+    this.img = srcImage;
+    this.xFrame = 0;
+    this.yFrame = 0;
+    this.destinationX = Food.location.xactual;
+    this.destinationY = Food.location.yactual;
+    this.frameSize = 79;
+    this.height = Food.size;
+    this.width = Food.size;
+    this.ticks = 5;
+    this.tickCounter = 0;
+    this.iFrame = 0;
+    this.numberOfFrames = 10;
+
+    this.render = function(){
+        this.xFrame = this.iFrame*this.frameSize;
+        ctx.drawImage(this.img, this.xFrame, this.yFrame, this.frameSize, this.img.height, this.destinationX, this.destinationY, this.height, this.width);
+    }
+
+    this.update = function(){
+        this.tickCounter++;
+        if(this.tickCounter > this.ticks - 1){
+            this.tickCounter = 0;
+            if(this.iFrame < this.numberOfFrames - 1){
+                this.iFrame+=1;
+            }else this.iFrame = 0;
+        }
+    }
+}
+
+var food = new sprite(foodImg);
